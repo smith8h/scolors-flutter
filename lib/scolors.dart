a69dd9d60461be61d57df4fbf9a278c706f61bc1
Dev. Smith <husseinshakir81@gmail.com>
2023-10-31 12:32:51 +0300

    feat(colors): add darker color generator

--- lib/scolors.dart
+++ lib/scolors.dart
@@ -1,7 +1,38 @@
 library scolors;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// A helpful class to get a degree of darker/lighter or opposite color provided
+/// by you.
+class SColors {
+  /// get a darker color from one color using a factor from 0-1.
+  static Color? darkerColor(
+    double factor, {
+    Color? color,
+    int? colorInt,
+    String? colorStr,
+  }) {
+    Color? col;
+    if (color != null) {
+      col = color;
+    } else if (colorInt != null) {
+      col = Color(colorInt);
+    } else if (colorStr != null) {
+      col = Color(int.parse(colorStr));
+    } else {
+      return null;
+    }
+
+    int alpha = col.alpha;
+    int red = col.red;
+    int green = col.green;
+    int blue = col.blue;
+
+    return Color.fromARGB(
+      alpha,
+      (red * factor).toInt().clamp(0, 255),
+      (green * factor).toInt().clamp(0, 255),
+      (blue * factor).toInt().clamp(0, 255),
+    );
+  }
 }
--- test/scolors_test.dart
+++ test/scolors_test.dart
@@ -1,12 +1,14 @@
-import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:scolors/scolors.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('DONE', () {
+    expect(SColors.darkerColor(.6), null);
+    expect(
+      SColors.darkerColor(.6, colorInt: 0xFF6A87E3),
+      const Color(0xff3f5188),
+    );
   });
 }
