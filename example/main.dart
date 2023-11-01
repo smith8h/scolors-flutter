2b266520a52ff3e5da6341b2647bd68c10ca9529
Dev. Smith <husseinshakir81@gmail.com>
2023-11-01 10:40:02 +0300

    doc(scolors): add example code

--- example/main.dart
+++ example/main.dart
@@ -9,10 +9,10 @@ void main() {
   Color? opposite = SColors.oppositeColor(colorStr: '#6a87e3');
   
   // get lighter color of a already defined color with factor:
-  Color? lighter = SColors.lighterColor(.3, colorStr: '#6a87e3');
+  Color? lighter = SColors.lighterColor(.3, colorInt: 0xFF6A87E3);
   
   // get darker color of an already defined color with factor:
-  Color? darker = SColors.darkerColor(.7, colorStr: '#6a87e3');
+  Color? darker = SColors.darkerColor(.7, color: const Color(0xff6a87e3));
   
   // get hsv color of an already defined color:
   Color? hsv = SColors.rgbToHsv(colorStr: '#6a87e3');
